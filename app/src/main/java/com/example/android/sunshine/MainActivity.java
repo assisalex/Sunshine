c22dbb03fa547b2609d326633cb741a55b58c552
alexdamianyassis <assis.alex.d@gmail.com>
2015-10-02 21:08:41 -0300

    Lesson 01 concluded

--- app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -1,16 +1,50 @@
 package com.example.android.sunshine;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import java.util.ArrayList;
+
+//public class MainActivity extends AppCompatActivity {
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<String> weekForecast;
+    private ArrayAdapter<String> adapterForecast;
+    private ListView lvForecast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        weekForecast = new ArrayList<String>();
+        weekForecast.add("Today - Sunny - 33 / 21");
+        weekForecast.add("Saturday - Cloudy - 36 / 19");
+        weekForecast.add("Sunday - Rainy - 35 / 21");
+        weekForecast.add("Monday - Rainy - 28 / 21");
+        weekForecast.add("Tuesday - Cloudy - 28 / 19");
+        weekForecast.add("Wednesday - Sunny - 31 / 19");
+        weekForecast.add("Thursday - Cloudy - 31 / 20");
+
+        adapterForecast = new ArrayAdapter<String>(
+                // The current context (this fragment's parent activity)
+                getBaseContext(),
+                // ID of list item layout
+                R.layout.list_item_forecast,
+                // ID of the textview to populate
+                R.id.list_item_forecast_textview,
+                // forecast data
+                weekForecast
+        );
+
+        lvForecast = (ListView) findViewById(R.id.listview_forecast);
+        lvForecast.setAdapter(adapterForecast);
+
+
     }
 
 
